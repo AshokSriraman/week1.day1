9a0e0c87af82c64d933eb870abbff1e90f20a32a
AshokSriraman <90105594+AshokSriraman@users.noreply.github.com>
2021-09-04 23:55:49 +0530

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
 	public static void main(String[] args) {
 		int input = 13;
 		boolean flag = false;
-		for (int i = 2; i < input; i++) {
+		for (int i = 2; i < input/2; i++) {
 			if (input % i == 0) {
 				flag = true;
 				break;
